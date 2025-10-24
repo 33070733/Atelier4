56f2aab5954832a72d21eb0b5ff884b5ac3ffd1b
33070733 <rabiabouhali01@gmail.com>
2025-10-24 00:01:33 -0400

    Commit 5 : Ajout de la méthode afficherCouleur()

--- Exercice1_Couleur.dart
+++ Exercice1_Couleur.dart
@@ -7,5 +7,12 @@ class Couleur {
 
   Couleur(this.r, this.g, this.b);
 
+  Couleur.rouge(: this(255, 0, 0);
+  Couleur.vert(: this(0, 255, 0);
+  Couleur.bleu(: this(0, 0, 255);
 
+  void afficherCouleur() {
+    print('(R : $r, V : $g, B : $b)');
+  }
+ 
 }
