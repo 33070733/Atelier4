5969d2256b067c855c0ecc8ec632b09b25860686
33070733 <rabiabouhali01@gmail.com>
2025-10-25 15:46:21 -0400

    Commit 2 : Ajout de la classe Livre

--- Exercice4_Media.dart
+++ Exercice4_Media.dart
@@ -11,3 +11,13 @@ class Media {
     print('Ceci est un média générique : $titre');
   }
 }
+class Livre extends Media {
+  final String auteur; // final comme demandé
+
+  Livre(String titre, this.auteur) : super(titre);
+
+  @override
+  void afficherType() {
+    print('Ceci est un Livre : $titre par $auteur');
+  }
+}
