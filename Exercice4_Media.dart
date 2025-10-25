27c4df2eca2de05889e8411b0ff8798c03cc0119
33070733 <rabiabouhali01@gmail.com>
2025-10-25 15:21:20 -0400

    Commit 3 : Ajout de la classe Livre et test initial

--- Exercice4_Media.dart
+++ Exercice4_Media.dart
@@ -22,3 +22,33 @@ class Livre extends Media {
     print('Ceci est un Livre : $titre par $auteur'); // format demandé
   }
 }
+// --- Commit 6 : Ajout de la classe Film et du catalogue complet ---
+class Film extends Media {
+  int dureeMinutes;
+
+  Film(String titre, this.dureeMinutes) : super(titre);
+
+  @override
+  void afficherType() {
+    print('🎬 Film : "$titre" (${dureeMinutes} minutes)');
+  }
+}
+
+// Fonction pour afficher le catalogue
+void afficherCatalogue(List<Media> catalogue) {
+  print('--- Catalogue de Médias ---');
+  for (var media in catalogue) {
+    media.afficherType();
+  }
+}
+
+// Fonction pour générer un catalogue exemple
+List<Media> genererCatalogue() {
+  return [
+    Livre('Le Petit Prince', 'Antoine de Saint-Exupéry'),
+    Film('Inception', 148),
+    Livre('1984', 'George Orwell'),
+    Film('Titanic', 195),
+    Livre('Harry Potter', 'J.K. Rowling'),
+  ];
+}
