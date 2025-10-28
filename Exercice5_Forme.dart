3d82a998660572926cc9f046f8ba601c925801c8
33070733 <rabiabouhali01@gmail.com>
2025-10-27 23:17:23 -0400

    Commit : Ajout de la classe Cercle avec implémentation de calculerAire()

--- Exercice5_Forme.dart
+++ Exercice5_Forme.dart
@@ -8,3 +8,30 @@ abstract class Forme {
     print("Calcul de l'aire...");
   }
 }
+// Classe Cercle 
+import 'dart:math';
+
+abstract class Forme {
+  double calculerAire();
+
+  void afficherMessage() {
+    print("Calcul de l'aire...");
+  }
+}
+
+class Cercle extends Forme {
+  final double rayon;
+
+  Cercle(this.rayon);
+
+  @override
+  double calculerAire() {
+    return pi * rayon * rayon;
+  }
+}
+
+void main() {
+  Cercle c = Cercle(5);
+  c.afficherMessage();
+  print("Aire du cercle : ${c.calculerAire()}");
+}
