f96aee031f694000dda2a40ad324f5867239f113
33070733 <rabiabouhali01@gmail.com>
2025-10-28 13:26:16 -0400

    Commit : Création de la classe abstraite Forme

--- Exercice5_Forme.dart
+++ Exercice5_Forme.dart
@@ -7,11 +7,3 @@ abstract class Forme {
 }
 
 // ici j'ai fais le Commit 3 – Création de la classe abstraite Forme
-class Cercle extends Forme {
-  final double rayon;
-  Cercle(this.rayon);
-
-  double calculerAire() => 3.14 * rayon * rayon;
-}
-
-// ici je faire le comit 5 - Classe Cercle avec calculerAire()
