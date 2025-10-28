5f0bf7374fb758a9564f7665c66ba3d9f8a12342
33070733 <rabiabouhali01@gmail.com>
2025-10-28 12:06:32 -0400

    _Forme.dart

--- Exercice5_Forme.dart
+++ Exercice5_Forme.dart
@@ -1,37 +1,12 @@
-// Création de la classe Forme
+// ----- Commit 1 : Classe abstraite Forme -----
+
+// Classe abstraite Forme
 abstract class Forme {
-  // Méthode abstraite sans corps
+  // Méthode abstraite (sans corps)
   double calculerAire();
 
   // Méthode concrète
   void afficherMessage() {
     print("Calcul de l'aire...");
   }
-}
-// Classe Cercle 
-import 'dart:math';
-
-abstract class Forme {
-  double calculerAire();
-
-  void afficherMessage() {
-    print("Calcul de l'aire...");
-  }
-}
-
-class Cercle extends Forme {
-  final double rayon;
-
-  Cercle(this.rayon);
-
-  @override
-  double calculerAire() {
-    return pi * rayon * rayon;
-  }
-}
-
-void main() {
-  Cercle c = Cercle(5);
-  c.afficherMessage();
-  print("Aire du cercle : ${c.calculerAire()}");
 }
