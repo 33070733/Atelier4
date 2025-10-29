08bd4735e0e793c926097d48abb3a8456910ed32
33070733 <rabiabouhali01@gmail.com>
2025-10-29 19:38:36 -0400

    Commit 2 : Ajout des méthodes move() et display()

--- Exercice2_Point.dart
+++ Exercice2_Point.dart
@@ -1,9 +1,20 @@
-// qst 1 : Création de la classe Point avec attributs privés _x et _y
+// question 1 : Création de la classe Point avec attributs privés _x et _y
 class Point {
   double _x;
   double _y;
 
-  // Commit 2 : Constructeur principal
+  // Commit apres question 2 : Ajout du constructeur principal
   Point(this._x, this._y);
+
+  // qst 3 : Ajout de la méthode move(dx, dy)
+  void move(double dx, double dy) {
+    _x += dx;
+    _y += dy;
+  }
+
+  // qst 4 apr cette etape un commit a faire : Ajout de la méthode display()
+  void display() {
+    print('Point(x: $_x, y: $_y)');
+  }
 }
 
