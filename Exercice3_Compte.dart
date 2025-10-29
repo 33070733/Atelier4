6e5ab1f718cadf471e8067c782d569ff92f0b2c9
33070733 <rabiabouhali01@gmail.com>
2025-10-29 11:03:29 -0400

    Commit 4 :creation du constructeur

--- Exercice3_Compte.dart
+++ Exercice3_Compte.dart
@@ -1,27 +1,7 @@
-
+// Commit 4 : Création du constructeur
 class Compte {
   double _solde = 0.0;
   final String numeroCompte;
 
   Compte(this.numeroCompte, [this._solde = 0.0]);
-
-  String get solde => '${_solde.toStringAsFixed(2)} \$';
-
-  void depot(double montant) {
-    if (montant > 0) {
-      _solde += montant;
-      print('Dépôt de $montant \$ effectué.');
-    } else {
-      print('Montant invalide.');
-    }
-  }
-
-  void retrait(double montant) {
-    if (montant > 0 && montant <= _solde) {
-      _solde -= montant;
-      print('Retrait de $montant \$ effectué.');
-    } else {
-      print('Retrait refusé (solde insuffisant ou montant invalide).');
-    }
-  }
 }
