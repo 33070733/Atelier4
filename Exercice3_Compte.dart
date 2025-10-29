98af564747b1ad22be016cfa86e58d8d259db003
33070733 <rabiabouhali01@gmail.com>
2025-10-29 11:10:57 -0400

    Commit 7 : Ajout de la fonction main() pour tester la classe Compte

--- Exercice3_Compte.dart
+++ Exercice3_Compte.dart
@@ -5,7 +5,9 @@ class Compte {
 
   Compte(this.numeroCompte, [this._solde = 0.0]);
   
-  String get solde => '${_solde.toStringAsFixed(2)} \$'; void depot(double montant) {
+  String get solde => '${_solde.toStringAsFixed(2)} \$'; 
+
+  void depot(double montant) {
     if (montant > 0) {
       _solde += montant;
       print('Dépôt de $montant \$ effectué.');
@@ -13,5 +15,17 @@ class Compte {
       print('Montant invalide.');
     }
   }
+}
 
+// --- Fonction main() à l’extérieur de la classe ---
+void main() {
+  print('--- Test Exercice 3 : Compte Bancaire ---');
+
+  var compte1 = Compte('12345');
+  print('Solde initial : ${compte1.solde}');
+
+  compte1.depot(200);
+  compte1.depot(-50);
+
+  print('Solde final : ${compte1.solde}');
 }
