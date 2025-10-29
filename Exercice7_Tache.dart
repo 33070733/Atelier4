bbd9a297c39573a48615cb5f8202900306e0e237
33070733 <rabiabouhali01@gmail.com>
2025-10-28 21:39:51 -0400

    Commit 4 : Ajout du constructeur Tache(this.description) qui incrémente nombreTotal

--- Exercice7_Tache.dart
+++ Exercice7_Tache.dart
@@ -1,9 +1,4 @@
-class Tache {
-  String description = "";
-   static int nombreTotal = 0;
-}
-// Commit 4 : Ajout du constructeur qui incrémente nombreTotal
-
+//exercice7..
 class Tache {
   String description = "";
   static int nombreTotal = 0;
