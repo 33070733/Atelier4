b89b0b41422674366c4e99ae7646e017c7a08f68
33070733 <rabiabouhali01@gmail.com>
2025-10-29 17:00:29 -0400

    Commit qst 2 : Ajout de la méthode move(dx, dy)

--- Exercice2_Point.dart
+++ Exercice2_Point.dart
@@ -1,27 +1,15 @@
 // Exercice 2 : Point dans le plan
 
+
+// question 1 : Création de la classe Point avec deux attributs privés _x et _y
 class Point {
   double _x;
   double _y;
 
-  // Constructeur
   Point(this._x, this._y);
-
-  // Déplace le point
+// ca qst 2 apr cette qst on vas faire un Commit : Ajout de la méthode move(dx, dy)
   void move(double dx, double dy) {
     _x += dx;
     _y += dy;
   }
-
-  // Affiche les coordonnées
-  void display() {
-    print('Point(x: $_x, y: $_y)');
-  }
-}
-
-void main() {
-  var pt = Point(2, 5);
-  pt.display();
-  pt.move(-1, 3);
-  pt.display();
 }
