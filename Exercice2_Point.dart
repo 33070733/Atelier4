1002299444d77a64d3f63223b7e6ca90f798bb40
33070733 <rabiabouhali01@gmail.com>
2025-10-29 19:38:06 -0400

    Commit 1 : Création de la classe Point avec attributs privés et constructeur

--- Exercice2_Point.dart
+++ Exercice2_Point.dart
@@ -1,25 +1,9 @@
-// Exercice 2 : Point dans le plan
-
-
-// question 1 : Création de la classe Point avec deux attributs privés _x et _y
+// qst 1 : Création de la classe Point avec attributs privés _x et _y
 class Point {
   double _x;
   double _y;
 
+  // Commit 2 : Constructeur principal
   Point(this._x, this._y);
-// ca qst 2 apr cette qst on vas faire un Commit : Ajout de la méthode move(dx, dy)
-  void move(double dx, double dy) {
-    _x += dx;
-    _y += dy;
-  }
-    void display() {
-    print("Coordonnées du point : ($_x, $_y)");
-  }
-  // Commit dans le question 4 : Test de la classe Point
-void main() {
-  var pt = Point(2, 5);
-  pt.display();     // Affiche : Coordonnées du point : (2.0, 5.0)
-  pt.move(-1, 3);
-  pt.display();     // Affiche : Coordonnées du point : (1.0, 8.0)
 }
-}
+
