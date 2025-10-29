69026f6529b760e4adfa84a3480df3b8ab36e5f0
33070733 <rabiabouhali01@gmail.com>
2025-10-28 22:13:52 -0400

    Commit 4 : Création d'objets Livre et affichage de leurs infos

---
+++ Exercice8_Livre.dart
@@ -0,0 +1,20 @@
+class Livre {
+  String titre = "";
+  String auteur = "";
+
+  Livre(this.titre, this.auteur);
+
+  void afficherInfos() {
+    print("Titre: $titre, Auteur: $auteur");
+  }
+}
+
+void main() {
+  var livre1 = Livre("Le Petit Prince", "Antoine de Saint-Exupéry");
+  var livre2 = Livre("1984", "George Orwell");
+  var livre3 = Livre("L'Étranger", "Albert Camus");
+
+  livre1.afficherInfos();
+  livre2.afficherInfos();
+  livre3.afficherInfos();
+}
